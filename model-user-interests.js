a1bb8f6d90078607ecf1db33ced040c28a30e23e
Jeff Meyerson <meff.jeyerson@gmail.com>
2017-12-03 11:54:11 -0800

    user interests and related links are enriched with tags

--- model-user-interests.js
+++ model-user-interests.js
@@ -6,49 +6,57 @@ db.then(() => {
   console.log('Connected correctly to server')
 })
 
+/*
+This script adds interests to the users database
+based on the posts the user upvoted.
+*/
+
 // Instantiate databases
 const votes = db.get('votes')
 const posts = db.get('posts')
-const users = db.get('users')
-const userInterests = db.get('users.interests')
+const userDB = db.get('users')
+const tags = db.get('tags')
 
+// Get tags in memory
+tags.find({})
+  .then(function(tags) {
+    let tagMap = {}
+    tags.forEach((tag) => {
+      if(tag.name.indexOf('.') == -1){
+        tagMap[tag.id] = tag.name
+      }
+    })
+    return tagMap
+  })
+  .then(function(tagMap){
 
-// Get the users
-users.find({})
-  .then(function (users) {
-    users.forEach(function(user){
-      console.log('user', user)
-      // Instantiate interests
-      const interests = {tag_interests : {}, category_interests : {}}
+    // Get the users
+    userDB.find({})
+      .then(function (users) {
+        users.forEach(function(user){
+          // Instantiate interests
+          const interests = {}
 
-      // Get upvotes
-      votes.find({$or : [{userId : user._id}, {userId : user._id.toString()}]})
-        .then(function (votes) {
-          //console.log('votes', votes)
-          // Get the posts associated with those upvotes
-          votes.forEach(function(vote){
-            posts.findOne({_id : ObjectId(vote.postId)})
-              .then(function (post){
-                console.log('tags', post.tags)
-                // Add tag interests
-                post.tags.forEach(function(tag){
-                  interests.tag_interests[tag] = 1
-                })
+          // Get upvotes
+          votes.find({$or : [{userId : user._id}, {userId : user._id.toString()}]})
+            .then(function (votes) {
 
-                // Add category interests
-                post.categories.forEach(function(category){
-                  interests.category_interests[category] = 1
-                })
+              // Get the posts associated with those upvotes
+              votes.forEach(function(vote){
+                posts.findOne({_id : ObjectId(vote.postId)})
+                  .then(function (post){
+                    // Add tag interests
+                    post.tags.forEach(function(tag){
+                      const tagName = tagMap[tag]
+                      interests[tagName] = 1
+                    })
 
-                // Add interests to db
-                userInterests.update({userId: user._id},
-                  { interests, userId : user._id },
-                  { upsert: true } )
+                    // Add interests to db
+                    userDB.update({_id: user._id},
+                      { $set: {interests : interests }} )
+                  })
+              })
             })
           })
         })
-      })
-    })
-
-// Get the tags associated with those episodes
-// Add the tags to user interests
+  })
---
+++ relatedlinks-add-tags.js
@@ -0,0 +1,55 @@
+require('dotenv').config();
+var ObjectId = require('mongodb').ObjectID;
+const  MetaInspector  = require('meta-scrape');
+const db = require('monk')(process.env.MONGO_DB);
+db.then(() => {
+  console.log('Connected correctly to server')
+})
+
+/*
+This script adds tags to the relatedLinks database
+based on the tags associated with the post of the related link.
+*/
+
+// Instantiate databases
+const relatedLinks = db.get('relatedlinks');
+const posts = db.get('posts')
+const tags = db.get('tags')
+
+// Get tags in memory
+tags.find({})
+  .then(function(tags) {
+    let tagMap = {}
+    tags.forEach((tag) => {
+      if(tag.name.indexOf('.') == -1){
+        tagMap[tag.id] = tag.name
+      }
+    })
+    return tagMap
+  })
+  .then(function(tagMap){
+    console.log('TAGMAP', tagMap)
+    // Get the related links
+    relatedLinks.find({})
+      .then(function (links) {
+        links.forEach(function(link){
+          // Instantiate the weights
+          const weights = {}
+
+          posts.findOne({_id : link.post})
+            .then(function (post) {
+              if(post){
+                // Add tag weights
+                post.tags.forEach(function(tag){
+                  const tagName = tagMap[tag]
+                  weights[tagName] = 1
+                })
+
+                // Add interests to db
+                relatedLinks.update({_id: link._id},
+                  { $set: { weights: weights }} )
+              }
+             })
+           })
+         })
+       })
