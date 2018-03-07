f93efb6445801662604b84b0e11550a5c2cf2839
jason <bjason@gmail.com>
2018-03-06 20:20:36 -0800

    Fixing script

--- migrateComments.js
+++ migrateComments.js
@@ -1,23 +1,23 @@
 require('dotenv').config()
 const db = require('monk')(process.env.MONGO_DB)
 const comments = db.get('comments')
-const getUrls = require('get-urls');
 const Bluebird = require('bluebird');
-const rp = require('request-promise');
 
 let promises = [];
-comment.find( {root: {$exists: false}})
+comments.find( {root: {$exists: false}})
   .each((comment) => {
     const postId = comment.post;
-    let promise = comment.update({id: comment.id}, {
+    let promise = comments.update({id: comment.id}, {
       $set: {
         root: postId,
-      },
+      }
     });
+    console.log('comment', comment);
     promises.push(promise);
   })
   .then(() => {
     return Bluebird.all(promises);
+    // return
   })
   .then(() => {
     console.log("done");
