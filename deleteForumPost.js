56ac1fcd46f8c372e46f2e40ea5eed6924c8e0c2
jason <bjason@gmail.com>
2018-03-26 09:08:24 -0700

    Delete form thread tool

---
+++ deleteForumPost.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const db = require('monk')(process.env.MONGO_DB)
+const forumthreads = db.get('forumthreads')
+const Bluebird = require('bluebird');
+
+let promises = [];
+forumthreads.find( {_id: "___________SOME_ID__HERE____"})
+  .each((entity) => {
+    console.log('entity', entity);
+    let promise = forumthreads.update({_id: entity._id}, {
+      $set: {
+        deleted: true,
+      }
+    });
+    console.log('entity', entity);
+    promises.push(promise);
+  })
+  .then(() => {
+    return Bluebird.all(promises);
+    // return
+  })
+  .then(() => {
+    console.log("done");
+    process.exit();
+  })
+  .catch((error) => { console.log(error); })
